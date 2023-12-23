b3c428035f4cb4691ca3fe421dc352cdb3ac8dcb
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-23 21:02:17 +0300

    init spring boot

---
+++ src/main/java/com/workintech/JavaS18ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaS18ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaS18ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/JavaS18ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaS18ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
