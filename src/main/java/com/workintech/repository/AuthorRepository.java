c38ed124f17ed5c16bdd5c5d3dac37a13017b79f
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-24 11:11:59 +0300

    created repository layer

---
+++ src/main/java/com/workintech/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.workintech.repository;
+
+import com.workintech.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+}
